27ea7cdb77d86d7753a3a672a6e03cd25279e647
DucJK <duchuynh330@gmail.com>
2023-04-11 09:55:52 +0700

    fix api supplier

--- src/app/core/service/supplier.service.ts
+++ src/app/core/service/supplier.service.ts
@@ -16,30 +16,30 @@ export class SupplierService {
   private urlAPI = 'http://localhost:3000';
   constructor(private http: HttpClient, private cookieService: CookieService) {}
   getSuppliers(searchvalue: any = null): Observable<Supplier[]> {
-    let url = `${this.urlAPI}/Supplier`;
+    let url = `${this.urlAPI}/Suppliers`;
     if (searchvalue != null) {
       url = `${url}?SupplierName_like=` + searchvalue;
     }
     return this.http.get<Supplier[]>(url, { headers: this.headers });
   }
   getByid(id: number): Observable<Supplier> {
-    let url = `${this.urlAPI}/Supplier/` + id;
+    let url = `${this.urlAPI}/Suppliers/` + id;
     return this.http.get<Supplier>(url, { headers: this.headers });
   }
   update(data: Supplier[], id: number): Observable<Supplier[]> {
-    let url = `${this.urlAPI}/Supplier/` + id;
+    let url = `${this.urlAPI}/Suppliers/` + id;
     return this.http.put<Supplier[]>(url, data, { headers: this.headers });
   }
   add(data: Supplier[]): Observable<Supplier[]> {
-    let url = `${this.urlAPI}/Supplier`;
+    let url = `${this.urlAPI}/Suppliers`;
     return this.http.post<Supplier[]>(url, data, { headers: this.headers });
   }
   delete(id: number): Observable<Supplier[]> {
-    let url = `${this.urlAPI}/Supplier/` + id;
+    let url = `${this.urlAPI}/Suppliers/` + id;
     return this.http.delete<Supplier[]>(url, { headers: this.headers });
   }
   getcountry(): Observable<Country[]> {
-    let url = `${this.urlAPI}/Country`;
+    let url = `${this.urlAPI}/Countries`;
     return this.http.get<Country[]>(url, { headers: this.headers });
   }
 }
